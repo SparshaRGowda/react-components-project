227461ac2af8a685a3f77829b91d3e08cf9af63a
Sparsha Ramakrishna <sparshar3@gmail.com>
2024-10-30 11:00:54 -0700

    adding comments on navigation comment for some styling changes

--- src/Components/NavigationMenuComp/NavigationMenu.jsx
+++ src/Components/NavigationMenuComp/NavigationMenu.jsx
@@ -3,10 +3,34 @@ import MenuList from "./MenuList";
 
 export default function NavigationMenu({ menus = [] }) {
   return (
-    <div
+    <nav
       style={{ width: "350px", height: "100vh", backgroundColor: "chocolate" }}
     >
       <MenuList list={menus} />
-    </div>
+    </nav>
   );
+}
+{
+  /* <nav
+  style={{
+    width: "100vw",
+    height: "50px",
+    backgroundColor: "chocolate",
+    position: "absolute",
+    top: 0,
+    display: "flex",
+    gap: "2rem",
+  }}
+>
+  <div>
+    <p>home</p>
+  </div>
+  <div>
+    <p>place1</p>
+  </div>
+  <div>
+    <p>place2</p>
+  </div>
+  //  <MenuList list={menus} /> 
+</nav>; */
 }
